58ab475d53479c75fbe85ff7df67ded7f033e730
Dallan Bhatti <dallan.bhatti@techtapir.com>
2020-09-08 13:27:46 -0700

    add credentials support to cors rules

--- src/__init__.py
+++ src/__init__.py
@@ -12,7 +12,7 @@ app.config.from_object("src.config.Config")
 # cache
 cache = Cache(app, config=app.config['REDIS_CONFIG'])
 # cors
-CORS(app)
+CORS(app, supports_credentials=True)
 # db
 db = SQLAlchemy(app)
 # migrate
