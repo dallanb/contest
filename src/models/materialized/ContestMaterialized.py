4c9c80224163100d6a30f7d23a67da31f2d0aacb
Dallan Bhatti <dallan.bhatti@techtapir.com>
2020-10-27 16:15:00 -0700

    moving over to laptop to give shoulder a break

---
+++ src/models/materialized/ContestMaterialized.py
@@ -0,0 +1,24 @@
+from sqlalchemy.dialects.postgresql import JSONB
+from sqlalchemy.ext.declarative import declared_attr
+from sqlalchemy_json import mutable_json_type
+from sqlalchemy_utils import UUIDType
+
+from ... import db
+from ...common.utils import camel_to_snake, time_now
+
+
+class ContestMaterialized(db.Model):
+    @declared_attr
+    def __tablename__(cls):
+        return camel_to_snake(cls.__name__)
+
+    uuid = db.Column(UUIDType(binary=False), primary_key=True, unique=True, nullable=False)
+    ctime = db.Column(db.BigInteger, default=time_now)
+    mtime = db.Column(db.BigInteger, onupdate=time_now)
+    name = db.Column(db.String, nullable=False)
+    status = db.Column(db.String, nullable=False)
+    avatar = db.Column(db.String, nullable=False)
+    participants = db.Column(mutable_json_type(dbtype=JSONB, nested=True))
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
