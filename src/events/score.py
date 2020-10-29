14d9dfa4aa74bade134cec3bb01bb6355758c852
Dallan Bhatti <dallan.bhatti@techtapir.com>
2020-10-29 13:48:53 -0700

    add score capability to contest materialized

---
+++ src/events/score.py
@@ -0,0 +1,20 @@
+import logging
+from ..services import (Contest as ContestService,
+                        ContestMaterialized as ContestMaterializedService,
+                        Participant as ParticipantService)
+
+
+class Score:
+    def __init__(self):
+        self.logger = logging.getLogger(__name__)
+        self.contest_service = ContestService()
+        self.contest_materialized_service = ContestMaterializedService()
+        self.participant_service = ParticipantService()
+
+    def handle_event(self, key, data):
+        if key == 'stroke_update':
+            contests = self.contest_materialized_service.find(uuid=data['contest_uuid'])
+            if contests.total:
+                contest = contests.items[0]
+                contest.participants[data['participant_uuid']]['score'] = data['strokes']
+                self.contest_materialized_service.save(instance=contest)
