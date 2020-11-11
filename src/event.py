72f0f7f5bdbe317dd6834fd9cb7e2d8e58d50c16
Dallan Bhatti <dallan.bhatti@techtapir.com>
2020-11-10 17:09:40 -0800

    updates for kafka producer

--- src/event.py
+++ src/event.py
@@ -1,16 +1,11 @@
-from . import app
 from .events import *
 
 
 def new_event_listener(event):
-    with app.app_context():
-        # start = time.perf_counter()
-        topic = event.topic
-        key = event.key
-        data = event.value
-        if topic == 'contests':
-            Contest().handle_event(key=key, data=data)
-        elif topic == 'scores':
-            Score().handle_event(key=key, data=data)
-        # finish = time.perf_counter()
-        # logging.info(f'This is the total time taken {round(finish - start, 2)}')
+    topic = event.topic
+    key = event.key
+    data = event.value
+    if topic == 'contests':
+        Contest().handle_event(key=key, data=data)
+    elif topic == 'scores':
+        Score().handle_event(key=key, data=data)
