17c964102aaf3d38c4704ae6881af0d699feceec
Dallan Bhatti <dallan.bhatti@techtapir.com>
2020-12-19 14:11:48 -0800

    update refres-db to reflrect changes in manage py

--- build/bin/refresh-db.sh
+++ build/bin/refresh-db.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-docker exec -it contest bash -c "python manage.py reset_db"
+docker exec -it contest bash -c "python manage.py reset"
 docker exec -it contest bash -c "python manage.py init"
